448be30a9b903e2be37632ef2b1436adca1b41ae
Naman Jain <134630688+FoundersMind@users.noreply.github.com>
2024-09-04 23:12:27 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# Social Network API Setup Instructions
+
+## Requirements
+
+- Docker
+- Docker Compose
+
+## Setup
+
+1. **Download and Extract the Package**
+
+   Download the provided package and extract it. You should have the following files:
+
+   - `docker-compose.yml`
+   - `setup.sh`
+   - `social_network_api_web_image.tar`
+   - `social_network_api_db_image.tar`
+
+2. **Run the Setup Script**
+
+   Open a terminal in the directory where the files are located and run:
+
+   ```bash
+   ./setup.sh
